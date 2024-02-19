7f2f8bc7a4ac9285bcbb14a3721b52c087c8f81d
Balwant Singh <balwant.singh@deorwine.com>
2024-02-19 18:06:16 +0530

    Update webhook.controller.js

--- controllers/webhook.controller.js
+++ controllers/webhook.controller.js
@@ -89,6 +89,9 @@ exports.addWebhook = (request, reply) => {
     type: request.params.type,
     values: request.body || {},
   };
+  console.log("request.body", request.body);
+  console.log("request.query", request.query);
+  console.log("request.params", request.params);
 
   models.webhook
     .create(data)
